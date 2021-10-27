b1b7fdd13a1bdc37028ac71735fb4c8bedc6310b
Kam <seekayel@users.noreply.github.com>
2021-10-27 10:49:45 -0400

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  console.log(JSON.stringify(req))
-  res.status(200).json({ name: 'John Doe' })
+  console.log(req.path)
+  res.status(200).json({ name: 'John Doe', env: process.env, path: req.path })
 }
